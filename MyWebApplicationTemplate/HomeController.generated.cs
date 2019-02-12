e002d4d698c255fa951c1aa15aead3be3d100549
Admin <Admin@IKAP-770001391>
2019-02-12 14:11:45 +0330

    Target framework changed to 472

--- MyWebApplicationTemplate/HomeController.generated.cs
+++ MyWebApplicationTemplate/HomeController.generated.cs
@@ -95,7 +95,9 @@ namespace MyWebApplicationTemplate.Controllers
             public _ViewNamesClass ViewNames { get { return s_ViewNames; } }
             public class _ViewNamesClass
             {
+                public readonly string Index = "Index";
             }
+            public readonly string Index = "~/Views/Home/Index.cshtml";
         }
     }
 
